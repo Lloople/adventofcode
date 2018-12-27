9d7bcfcc2c36ea2c1cb3c3f61d4430ea6be1e592
Lloople <hello@davidllop.com>
2018-12-27 14:09:07 +0100

    Solve 2015 day 3 part 2

--- src/Y2015/D03.php
+++ src/Y2015/D03.php
@@ -28,28 +28,68 @@ class D03 extends Day
         $deliveries = ["0,0" => 1];
 
         foreach (str_split($this->input) as $movement) {
-            if ($movement === '^') {
-                $marker[1]++;
-            } elseif ($movement === 'v') {
-                $marker[1]--;
-            } elseif ($movement === '<') {
-                $marker[0]--;
-            } elseif ($movement === '>') {
-                $marker[0]++;
-            }
+            $marker = $this->moveMarker($marker, $movement);
 
-            if (! isset($deliveries["{$marker[0]},{$marker[1]}"])) {
-                $deliveries["{$marker[0]},{$marker[1]}"] = 0;
-            }
-
-            $deliveries["{$marker[0]},{$marker[1]}"]++;
+            $deliveries = $this->incrementDeliveries($deliveries, $marker);
         }
 
         return count($deliveries);
     }
 
+    /**
+     * --- Part Two ---
+     *
+     * The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to deliver presents with him.
+     *
+     * Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+     *
+     * This year, how many houses receive at least one present?
+     *
+     * For example:
+     *
+     * ^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.
+     * ^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.
+     * ^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.
+     */
     public function part2()
     {
-        // TODO: Implement part2() method.
+        $markers = [[0, 0], [0, 0]];
+        $deliveries = ["0,0" => 1];
+
+        foreach (str_split($this->input, 2) as $movements) {
+            foreach (str_split($movements) as $santaOrRobot => $movement) {
+                $markers[$santaOrRobot] = $this->moveMarker($markers[$santaOrRobot], $movement);
+
+                $deliveries = $this->incrementDeliveries($deliveries, $markers[$santaOrRobot]);
+            }
+        }
+
+        return count($deliveries);
+    }
+
+    private function moveMarker(array $marker, $movement)
+    {
+        if ($movement === '^') {
+            $marker[1]++;
+        } elseif ($movement === 'v') {
+            $marker[1]--;
+        } elseif ($movement === '<') {
+            $marker[0]--;
+        } elseif ($movement === '>') {
+            $marker[0]++;
+        }
+
+        return $marker;
+    }
+
+    private function incrementDeliveries(array $deliveries, array $marker)
+    {
+        if (! isset($deliveries["{$marker[0]},{$marker[1]}"])) {
+            $deliveries["{$marker[0]},{$marker[1]}"] = 0;
+        }
+
+        $deliveries["{$marker[0]},{$marker[1]}"]++;
+
+        return $deliveries;
     }
 }
